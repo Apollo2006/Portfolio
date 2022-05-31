8f5116b7ef7278fc335987d208b7a337d85eb7b0
Apollo2006 <82603643+Apollo2006@users.noreply.github.com>
2022-05-31 15:57:57 -0400

    Add array of obj and mapping to display

--- src/Components/Shared/ProjectCard.jsx
+++ src/Components/Shared/ProjectCard.jsx
@@ -1,11 +1,22 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Code } from "@chakra-ui/react"
+import PropTypes from 'prop-types'
+import GithubBTN from "./GithubBTN"
 
 
-function ProjectCard({ header, span }) {
+function ProjectCard({ header, description }) {
   return (
-    <Box textAlign='center' fontWeight='semibold' fontSize='2xl'>{header}</Box>
+    <>
+      <Box textAlign='left' justifyContent='center' fontWeight='thin' m='3% 10%'>
+        <h1 ><Code fontSize='2xl' fontWeight='thin'>{header}</Code><GithubBTN/></h1>
+        <p>{description}</p>
+      </Box>
+    </>
     
   )
+}
+
+ProjectCard.propTypes = {
+  header: PropTypes.string.isRequired 
 }
 
 export default ProjectCard
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,19 +2,36 @@ import { SimpleGrid, Box } from '@chakra-ui/react'
 import ProjectCard from '../Components/Shared/ProjectCard'
 
 function Projects() {
+
+  const Projects = [
+    {
+      "header" : "<h1>Portfolio</h1>",
+      "desc": "The current website made with React and Chakra UI. Built with the knowledge I have gained till now."
+    },
+    {
+      "header" : "<h1>FeedbackUI</h1>",
+      "desc": "This is a basic FeedbackUI that could be used in order to get reviews from customer. Mainly the use of filter and mapping."
+    },
+    {
+      "header" : "<h1>GithubFinder</h1>",
+      "desc": "This application can find Github users and their info with the use of Github API. P.S I had outside help."
+    },
+    {
+      "header" : "<h1>ToDo.V2</h1>",
+      "desc": "While this is a very basic and old project. I fondly look back at this as my starting step towards coding and look at how much progress is made."
+    }
+  ]
+
+
   return (
-    <div justify='center' spacing='10' w='100%'>
+    <div spacing='10' w='100%'>
         <Box 
           bg='white' mt='5%'
           textAlign='center' justify='center'  fontWeight='thin' fontFamily='serif' letterSpacing='wider' fontSize='4xl'>
             Projects
         </Box>
-        <SimpleGrid columns={2} justifyContent='center'>
-          <ProjectCard header='Portfolio(A.K.A: this site)' />
-          <ProjectCard header='FeedbackUI' />
-          <ProjectCard header='GithubFinder' />
-          <ProjectCard header='test' />
-          
+        <SimpleGrid justifyContent='center' columns={1}  >
+          {Projects.map(Projects => <ProjectCard header={Projects.header} description={Projects.desc}/>)}
         </SimpleGrid>
     </div>
   )
